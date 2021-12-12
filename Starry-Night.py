8054233717ca6515cc01b904cb9790e927145f90
Abhinav Anand <cyberaka@gmail.com>
2021-12-13 01:21:23 +0530

    Committing 3 programs. Caterpillar doesn't work yet properly. Needs to be debugged.

---
+++ Starry-Night.py
@@ -0,0 +1,32 @@
+import turtle as t
+from random import randint, random
+
+
+# Draw star function
+def draw_star(points, size, col, x, y):
+    print("draw_star(points={0}, size={1}, col={2}, x={3}, y={4})".format(points, size, col, x, y))
+    t.penup()
+    t.goto(x, y)
+    t.pendown
+    angle = 180 - (180 / points)
+    t.color(col)
+    t.begin_fill()
+    for i in range(points):
+        t.forward(size)
+        t.right(angle)
+    t.end_fill()
+
+
+# Main code
+t.Screen().bgcolor('dark blue')
+# t.hideturtle()
+
+# Infinite loop to draw random stars
+while True:
+    ranPts = randint(2, 5) * 2 + 1
+    ranSize = randint(10, 50)
+    ranCol = (random(), random(), random())
+    ranX = randint(-350, 300)
+    ranY = randint(-250, 250)
+
+    draw_star(ranPts, ranSize, ranCol, ranX, ranY)
